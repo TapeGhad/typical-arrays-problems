7033a8eea0f5194a5aaf26f22fb65bce53fac25f
tapeghad <tapeghad@gmail.com>
2020-09-14 15:18:15 +0300

    feat: finish min max avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined) return 0;
+  if (array.length === 0) return 0;
+  return array.reduce((minimal, elem) => {
+    if (elem < minimal) return elem;
+    else return minimal;
+  }, array[0]);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined) return 0;
+  if (array.length === 0) return 0;
+  return array.reduce((maximal, elem) => {
+    if (elem > maximal) return elem;
+    else return maximal;
+  }, array[0]);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined) return 0;
+  if (array.length === 0) return 0;
+  let sum = array.reduce((avg, elem) => {
+    return avg + elem;
+  }, 0);
+  return sum / array.length;
 }
